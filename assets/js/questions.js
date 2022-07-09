439964ab532a4100201cf9318acba9a48b071f38
aurumbright <aurum.elean@gmail.com>
2022-07-09 10:46:09 -0700

    first commit

---
+++ assets/js/questions.js
@@ -0,0 +1,36 @@
+// TODO: store all questions here
+// NOTE: hint - can use an array that contains objects
+/* 
+Step 1: Write out 5 multiple choice questions about javascript and their options and correct answers
+Step 2: Arrange into array of objects to const questions
+*/
+
+const questions = [
+    {
+        ask: "",
+        choices: ["", "", "", ""],
+        correct: ""
+    },
+    {
+        ask: "",
+        choices: ["", "", "", ""],
+        correct: ""
+    },
+    {
+        ask: "",
+        choices: ["", "", "", ""],
+        correct: ""
+    }.
+    {
+        ask: "",
+        choices: ["", "", "", ""],
+        correct: ""
+    },
+    {
+        ask: "",
+        choices: ["", "", "", ""],
+        correct: ""
+    },
+];
+
+
